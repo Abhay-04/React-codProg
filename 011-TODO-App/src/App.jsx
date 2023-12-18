e7e0703bf6b41222c4a87165fb8737c91befa8d3
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2023-12-18 11:02:30 +0530

    .

--- 011-TODO-App/src/App.jsx
+++ 011-TODO-App/src/App.jsx
@@ -3,39 +3,40 @@ import TodoForm from "./TodoForm";
 import Todos from "./Todos";
 
 function App() {
-  const [todos , setTodos] = useState([
-    {id:1, title: "Learn JS", completed: true},
-    {id:2, title: "Learn React", completed: false},
-    {id:3, title: "Learn Github", completed: true},
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Learn JS", completed: true },
+    { id: 2, title: "Learn React", completed: false },
+    { id: 3, title: "Learn Github", completed: true },
   ]);
 
-  function addTodo(newTodo){
-    setTodos(prevState => [...prevState , newTodo])
-
+  function addTodo(newTodo) {
+    setTodos((prevState) => [...prevState, newTodo]);
   }
 
-  function removeTodo(id){
-    setTodos(prevState => prevState.filter((todo)=> todo.id !== id))
+  function removeTodo(id) {
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
   }
 
-  function toggleCompleted(id){
-    setTodos(prevState => {
-      return prevState.map((todo)=>{
-        if(todo.id===id){
-          
-          return {...todo , completed: !todo.completed}
-        }else{
+  function toggleCompleted(id) {
+    setTodos((prevState) => {
+      return prevState.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
           return todo;
         }
-      })
-    })
-
+      });
+    });
   }
   return (
     <>
       <h1>TODO LIST</h1>
-      <TodoForm addTodo = {addTodo} />
-      <Todos todos = {todos} toggleCompleted = {toggleCompleted} removeTodo = {removeTodo}/>
+      <TodoForm addTodo={addTodo} />
+      <Todos
+        todos={todos}
+        toggleCompleted={toggleCompleted}
+        removeTodo={removeTodo}
+      />
     </>
   );
 }
