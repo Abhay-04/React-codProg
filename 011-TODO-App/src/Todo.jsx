36f0684ec5b2bca587bc653ae244b0cc41b96627
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2023-12-18 17:45:47 +0530

    style : input overflow fixed

--- 011-TODO-App/src/Todo.jsx
+++ 011-TODO-App/src/Todo.jsx
@@ -5,28 +5,25 @@ function Todo({ id, title, completed, toggleCompleted, removeTodo }) {
   return (
     <div className="todo">
       <div className="todo__title">
-      <input
-        type="checkbox"
-        checked={completed}
-        onChange={() => {
-          return toggleCompleted(id);
-        }}
-      />
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() => {
+            return toggleCompleted(id);
+          }}
+        />
 
-      <p className={`${completed ? "completed" : ""}`}>{title}</p>
-
-
+        <p className={`${completed ? "completed" : ""}`}>{title}</p>
       </div>
 
-      <div className="cross__btn" onClick={() => {
+      <div
+        className="cross__btn"
+        onClick={() => {
           return removeTodo(id);
-        }}>
-          <ImCross />
-
+        }}
+      >
+        <ImCross />
       </div>
-      
-      
-      
     </div>
   );
 }
